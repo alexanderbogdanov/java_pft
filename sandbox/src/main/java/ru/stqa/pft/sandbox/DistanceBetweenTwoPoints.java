3f252cfc824d90d7cceaf90af3d41691f047f9df
alexanderbogdanovgit config --global user.name alexanderbogdanovgit config --global user.name alexanderbogdanov <ironweed@list.ru>
2020-12-03 23:23:10 +0300

    Test framework set up and DistanceBetweenTwoPoints tests added (ex 3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenTwoPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenTwoPoints.java
@@ -4,6 +4,9 @@ public class DistanceBetweenTwoPoints {
   public static void main(String[] args) {
     Point p1 = new Point(9, 7);
     Point p2 = new Point(3, 2);
-    System.out.println("The distance between points " + "a (x1 = " + p1.x + ", y1 = " + p1.y + ") and " + "b (x2 = " + p2.x + ", y2 = " + p2.y + ") equals " + p1.distance(p2));
+    System.out.println("The distance between points "
+            + "a (x1 = " + p1.x + ", y1 = " + p1.y + ") and "
+            + "b (x2 = " + p2.x + ", y2 = " + p2.y + ") equals " + p1.distance(p2));
   }
 }
+
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.xml.dom.Tag;
 
 public class PointTests {
 
